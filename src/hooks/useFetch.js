03de83f38001c42a4216842893e4d0d4faa0ce37
GuangyuanTang <254202042@qq.com>
2024-06-30 11:56:03 +0800

    feat: add options to useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const useFetch = (url) => {
+const useFetch = (url, options = {}) => {
     const [data, setData] = useState()
     const [isError, setIsError] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
@@ -13,16 +13,25 @@ const useFetch = (url) => {
         setIsLoading(true)
 
         fetch(url, {
-            signal: controller.signal
+            signal: controller.signal,
+            ...options
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json()
+                }
+                return Promise.reject(res)
+            })
             .then(setData)
             .catch((err) => {
                 if (err.name !== 'AbortError') {
                     setIsError(true)
                 }
             })
-            .finally(() => setIsLoading(false))
+            .finally(() => {
+                if (controller.signal.aborted) return
+                setIsLoading(false)
+            })
 
         return () => controller.abort()
     }, [url])
