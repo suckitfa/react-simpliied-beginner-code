462eb470e7c6b4d6e034cca71d77ad83aac139f6
GuangyuanTang <254202042@qq.com>
2024-06-30 11:54:13 +0800

    feat: useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,30 +5,27 @@ const useFetch = (url) => {
     const [isError, setIsError] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
 
-    // const controller = new AbortController()
+    const controller = new AbortController()
 
-    // useEffect(() => {
-    //     setData(undefined)
-    //     setIsError(false)
-    //     setIsLoading(true)
+    useEffect(() => {
+        setData(undefined)
+        setIsError(false)
+        setIsLoading(true)
 
-    //     fetch(url, {
-    //         signal: controller.signal
-    //     })
-    //         .then(res => res.json())
-    //         .then(d => {
-    //             console.log('setData = ', d)
-    //             setData(d)
-    //         })
-    //         .catch((err) => {
-    //             if (err.name !== 'AbortError') {
-    //                 setIsError(true)
-    //             }
-    //         })
-    //         .finally(() => setIsLoading(false))
+        fetch(url, {
+            signal: controller.signal
+        })
+            .then(res => res.json())
+            .then(setData)
+            .catch((err) => {
+                if (err.name !== 'AbortError') {
+                    setIsError(true)
+                }
+            })
+            .finally(() => setIsLoading(false))
 
-    //     return () => controller.abort()
-    // }, [url])
+        return () => controller.abort()
+    }, [url])
     return { data, isError, isLoading }
 }
 
